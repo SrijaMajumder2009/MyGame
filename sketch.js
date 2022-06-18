a96ba6c0bdefe313502f8682271a70014e0905d1
SrijaMajumder2009 <90127383+SrijaMajumder2009@users.noreply.github.com>
2022-06-18 12:11:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var backgr
+
+function preload() {
+  backgr = loadImage("./Images/backg11.png")
+}
+
+function setup() {
+  createCanvas(1500,700)
+}
+
+function draw() {
+  background(backgr);
+  
+}
+
